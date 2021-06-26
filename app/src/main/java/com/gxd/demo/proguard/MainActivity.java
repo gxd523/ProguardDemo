afa386c4d8492fa8619e1a6181f254cd7966e38d
郭晓东 <guoxiaodong@corp.netease.com>
2021-06-26 20:13:34 +0800

    upload code

---
+++ app/src/main/java/com/gxd/demo/proguard/MainActivity.java
@@ -0,0 +1,11 @@
+package com.gxd.demo.proguard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
